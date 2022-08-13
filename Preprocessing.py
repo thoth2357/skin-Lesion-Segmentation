7cf9c763392b33e2948168f16752a2864e866015
Thoth <98170427+thoth2357@users.noreply.github.com>
2022-08-13 19:32:52 +0100

    created method to convert images to array after opening them

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,6 +1,8 @@
 #importing modules
 import os
 import glob
+import numpy as np
+from PIL import Image
 
 class Preprocessing():
     def __init__(self, dataset_folder_path:str) -> None:
@@ -13,5 +15,14 @@ class Preprocessing():
         """
         files_orig_image = sorted(filter(os.path.isfile, glob.glob(self.DATASET_FOLDER_PATH + "/org data/*.png")))
         files_ground_truth = sorted(filter(os.path.isfile, glob.glob(self.DATASET_FOLDER_PATH + "/GT/*.png")))
-        
+        return files_orig_image, files_ground_truth
+    
+    def convert_images_to_array(self, files_orig_image, files_ground_truth):
+        """
+        Converts the images to arrays.
+        """
+        self.X_train = np.array([np.array(Image.open(fname)) for fname in files_orig_image])
+        self.Y_train = np.array([np.array(Image.open(fname)) for fname in files_ground_truth])
+        return self.X_train, self.Y_train
+    
         
