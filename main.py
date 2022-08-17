5fbd73d0dc7d49d2bdf43351b06e4ca6ac0b6346
Thoth <98170427+thoth2357@users.noreply.github.com>
2022-08-17 11:50:29 +0100

    splitting method initialized

--- main.py
+++ main.py
@@ -16,5 +16,7 @@ def Main():
     #converting the images to arrays
     X_train, Y_train = dataset_processing.convert_images_to_array(files_orig_image, files_ground_truth)
 
+    #splitting the dataset into train and test set
+    x_train, x_test, y_train, y_test = dataset_processing.train_test_split_image(X_train, Y_train, test_size=0.2)
 if __name__ == '__main__':
     Main()
