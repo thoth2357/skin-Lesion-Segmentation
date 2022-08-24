89f51e77eb1f41d51e281ed80a17bb67f9723904
thoth2357 <thoth2357@gmail.com>
2022-08-24 06:50:37 +0100

    added loop for segmenting images

--- Matlab-Version/main.m
+++ Matlab-Version/main.m
@@ -5,8 +5,21 @@ Folder_GT_images       = 'GT';
 % Defining the folder for the Ground Truth Images
 
 Skin_images_Store 	   = imageDatastore(Folder_org_images,'IncludeSubfolders',true,'LabelSource','foldernames'); 
-% Create datastore for large collections of data
+% Create image datastore for storing images data
 
 Skin_Seg_images_Store    = imageDatastore(Folder_GT_images  ,'IncludeSubfolders',true,'LabelSource','foldernames'); 
-% Create datastore for large collections of data
+% Create datastore for storing images data
+
+original_image_files    = Skin_images_Store.Files;    
+% Get all Files Of Skin_images_Store datastore into variable 
+
+ground_truth_image_files = Skin_Seg_images_Store.Files; 
+% Get all Files Of Skin_Seg_images_Store datastore into variable
+
+for i = 1:numel(original_image_files)           % Looping through to process all image
+    Image_name    = original_image_files{i};    % Get org image By Indexing our Files from datastore  
+    SegImage_name = ground_truth_image_files{i}; % Get org image By Indexing our Files from datastore 
+    [original_image,normalized_image,DS_SCORE] =Segmentation_function(Image_name,SegImage_name,i);
+    DS(i) = DS_SCORE;
+end
 
